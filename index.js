a84f5ee695637c519edae5d4946931bad5cc3a0d
henrychu04 <henrychu04@outlook.com>
2020-05-30 21:48:57 -0400

    added sync, updated add, edit, list

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const inquirer = require('inquirer');
+require('dotenv').config();
 inquirer.registerPrompt('datetime', require('inquirer-datepicker-prompt'));
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const ObjectId = require('mongodb').ObjectID;
 
 async function main() {
     try {
@@ -18,29 +20,38 @@ async function main() {
                     'Add an item',
                     'Edit an item',
                     'Delete an item',
-                    'List all items'
+                    'List all items',
+                    'Sync'
                 ]
             },
             {
                 type: 'input',
-                name: 'edit_item',
-                message: 'Select an item to edit',
-                when: function(answers) {
-                    return answers.action == 'Edit an item';
-                }
-            },
-            {
-                type: 'input',
                 name: 'delete_item',
-                message: 'Input id to delete',
+                message: 'Input id to delete:',
                 when: function(answers) {
                     return answers.action == 'Delete an item';
                 }
             },
             {
+                type: 'input',
+                name: 'name',
+                message: 'Name:',
+                when: function(answers) {
+                    return answers.action == 'Add an item';
+                }
+            },
+            {
+                type: 'datetime',
+                name: 'date',
+                message: 'Date:',
+                when: function(answers) {
+                    return answers.action == 'Add an item';
+                }
+            },
+            {
                 type: 'list',
                 name: 'type',
-                message: 'Type',
+                message: 'Type:',
                 choices: [
                     'Expense',
                     'Deposit'
@@ -50,62 +61,40 @@ async function main() {
                 }
             },    
             {
-                type: 'datetime',
-                name: 'date',
-                message: 'Date',
-                when: function(answers) {
-                    return answers.action == 'Add an item';
-                }
-            },
-            {
-                type: 'input',
-                name: 'name',
-                message: 'Name',
-                when: function(answers) {
-                    return answers.action == 'Add an item';
-                }
-            },
-            {
                 type: 'number',
-                name: 'e_amount',
-                message: 'Amount',
+                name: 'expense',
+                message: 'Expense amount:',
                 when: function(answers) {
                     return answers.type == 'Expense';
                 }  
             },
             {
                 type: 'number',
-                name: 'd_amount',
-                message: 'Amount',
+                name: 'deposit',
+                message: 'Deposit amount:',
                 when: function(answers) {
                     return answers.type == 'Deposit';
                 }
             }
-            
         ])
 
         console.log(answers);
 
         if (answers.action == 'Add an item') {
-            date = answers.date.getDate().toString();
-            month = (answers.date.getMonth() + 1).toString();
-            year = answers.date.getFullYear().toString();
-            full_date = `${month}/${date}/${year}`;
-
-            console.log(full_date);
-
-            await insertItem(client, answers, full_date);
+            await insertItem(client, answers);
         } else if (answers.action == 'Edit an item') {
-           
+            await editItem(client);
         } else if (answers.action == 'Delete an item') {
 
         } else if (answers.action == 'List all items') {
             await list_all(client);
+        } else if (answers.action == 'Sync') {
+            await sync(client);
         }
     } catch (e) {
         console.error(e);
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 
@@ -115,79 +104,75 @@ async function list_all(client) {
     db = client.db('inventory');
     collection = db.collection('items');
     
-    all_items = [];
-    items = await collection.find().toArray();
-
-    for (i = 0; i < items.length; i++) {
-        console.log(items[i]);
-    }
+    console.log(await collection.find().toArray());
 }
 
-async function addToMonth(client, item, id, month) {
+async function addToMonth(client, item, id) {
     db = client.db('inventory');
     collection = db.collection('month');
 
-    if (item.expense == 0) {
+    var id = item._id;
+    var month = parseInt(item.date.split('/')[0]);
+
+    if (item.expense == null) {
         await collection.insertOne({
-            month: {
-                deposit: {
-                    _id: id
-                }
+            month: month,
+            deposit: {
+                _id: id
             }
         })
     } else {
         await collection.insertOne({
-            month: {
-                expense: {
-                    _id: id
-                }
+            month: month,
+            expense: {
+                _id: id
             }
         })
     }
 }
 
-async function sync(client, item, id) {
-    month = parseInt(item.date.split('/')[0]);
+async function sync(client) {
+    try {
+        db = client.db('inventory');
+        items_collection = db.collection('items');
+        month_collection = db.collection('month');
 
-    switch (month) {
-        case 1:
-            await addToMonth(client, item, id, 1);
-            break;
-        case 2:
-            await addToMonth(client, item, id, 2);
-            break;
-        case 3:
-            await addToMonth(client, item, id, 3);
-            break;
-        case 4:
-            await addToMonth(client, item, id, 4);
-            break;
-        case 5:
-            await addToMonth(client, item, id, 5);
-            break;
-        case 6:
-            await addToMonth(client, item, id, 6);
-            break;
-        case 7:
-            await addToMonth(client, item, id, 7);
-            break;
-        case 8:
-            await addToMonth(client, item, id, 8);
-            break;
-        case 9:
-            await addToMonth(client, item, id, 9);
-            break;
-        case 10:
-            await addToMonth(client, item, id, 10);
-            break;
-        case 11:
-            await addToMonth(client, item, id, 11);
-            break;
-        case 12:
-            await addToMonth(client, item, id, 12);
-            break;
-        default:
-            break;
+        var colls = await db.listCollections().toArray();
+        var month_exist = false;
+
+        for (var i = 0; i < colls.length; i++) {
+            if (coll[i].name === 'month') month_exist = true;
+        }
+
+        if (month_exist) {
+            await items_collection.find().forEach(async function(item) {
+                var match = false;
+
+                await month_collection.find().forEach(function(month) {
+                    if (month.expense == undefined) {
+                        if (month.deposit._id.equals(item._id)) {
+                            match = true;
+                        }
+                    } else if (month.deposit == undefined) {
+                        if (month.expense._id.equals(item._id)) {
+                            match = true;
+                        }
+                    }
+                });
+
+                if (!match) {
+                    await addToMonth(client, item, item._id);
+                }
+            });
+        } else {
+            await items_collection.find().forEach(async function(item) {
+                await addToMonth(client, item, item._id);
+            });
+        }
+    } catch (err) {
+        console.log(err);
+    } finally {
+        console.log('Synced');
     }
 }
 
@@ -195,57 +180,110 @@ async function deleteItem(client) {
     db = client.db('inventory');
     collection = db.collection('items');
 
-    query = { name: 'Test' };
-    item = await collection.findOne(query);
-
-    await collection.deleteOne(query);
-
-    await collection.find().toArray((err, items) => {
-        console.log(items);
-    });
+    
 }
 
 async function editItem(client) {
     db = client.db('inventory');
     collection = db.collection('items');
+    console.log(await collection.find().toArray());
 
-    query = { name: 'Test' };
-    newVal = { $set: { date: '12/03/2019'} };
+    edits = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Input id to edit:'
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter new name:'
+        },
+        {
+            type: 'datetime',
+            name: 'date',
+            message: 'Enter new date:'
+        },
+        {
+            type: 'list',
+            name: 'type',
+            message: 'Select new type:',
+            choices: [
+                'Expense',
+                'Deposit'
+            ]
+        },
+        {
+            type: 'number',
+            name: 'expense',
+            message: 'Enter new expense amount:',
+            when: function(edits) {
+                return edits.type == 'Expense';
+            }
+        },
+        {
+            type: 'number',
+            name: 'deposit',
+            message: 'Enter new deposit amount:',
+            when: function(edits) {
+                return edits.type == 'Deposit';
+            }
+        }
+    ])
 
-    await collection.updateOne(query, newVal);
+    var old = await collection.findOne(
+        { _id: new ObjectId(edits.id) }
+    );
 
-    await collection.find().toArray((err, items) => {
-        console.log(items);
-    });
+    console.log(old);
+
+    var month = (edits.date.getMonth() + 1).toString();
+    var date = edits.date.getDate().toString();
+    var year = edits.date.getFullYear().toString();
+    var full_date = `${month}/${date}/${year}`;
+
+    if (old.date.split('/')[0] != month) {
+        month_collection = db.collection('month');
+
+
+    }
+
+    // await collection.findOneAndReplace(
+    //     { _id: new ObjectId(edits.id) },
+    //     {
+    //         'name': edits.name,
+    //         'date': full_date,
+    //         'expense': edits.expense,
+    //         'deposit': edits.deposit
+    //     }
+    // );
+
+    console.log(await collection.find().toArray());
 }
 
-async function insertItem(client, answers, full_date) {
+async function insertItem(client, answers) {
     db = client.db('inventory');
     collection = db.collection('items');
 
+    var month = (answers.date.getMonth() + 1).toString();
+    var date = answers.date.getDate().toString();
+    var year = answers.date.getFullYear().toString();
+    var full_date = `${month}/${date}/${year}`;
+
     item = {
         name: answers.name,
-        expense: answers.e_amount,
-        deposit: answers.d_amount,
-        date: full_date
+        date: full_date,
+        expense: answers.expense,
+        deposit: answers.deposit,
     }
 
     id = '';
 
-    await collection.insertOne(item, function(err, result) {
+    await collection.insertOne(item, function(result) {
         id = result.insertedId
     });
 
-    await sync(client, item, id);
+    await sync(client);
 
-    await collection.find().toArray((err, items) => {
-        console.log(items);
-    });
+    console.log(await collection.find().toArray());
 }
-
-async function listDatabases(client) {
-    databasesList = await client.db().admin().listDatabases();
-
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
